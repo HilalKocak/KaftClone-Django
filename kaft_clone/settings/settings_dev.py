a89155da640e7b8d84f246d1ab1682800e6d4ce9
Hilal38 <hhilalkocak@gmail.com>
2023-05-27 19:23:38 +0300

    settings splitted

---
+++ kaft_clone/settings/settings_dev.py
@@ -0,0 +1,12 @@
+import os
+from .settings_base import BASE_DIR, INSTALLED_APPS
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, '../db.sqlite3'),
+    }
+}
+if os.environ.get('DJANGO_DEBUG')== 'True':
+    INSTALLED_APPS += [
+        'django_extensions'
+        ]
